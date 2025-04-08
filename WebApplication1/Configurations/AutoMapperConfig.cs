c8f91a856c1390898af2f8f46081a00865edf1d7
SATISHKUMAR02 <133092141+SATISHKUMAR02@users.noreply.github.com>
2025-04-08 16:22:01 +0530

    Update AutoMapperConfig.cs

--- WebApplication1/Configurations/AutoMapperConfig.cs
+++ WebApplication1/Configurations/AutoMapperConfig.cs
@@ -9,9 +9,9 @@ namespace WebApplication1.Configurations
     {
         public AutoMapperConfig()
         {
-            CreateMap<Student, Studentdto>().ReverseMap(); // allow bi-directional mapping 
-            CreateMap<Student, Student>().ReverseMap();
-            CreateMap<RoleDto, Role>().ReverseMap();
+            CreateMap<Student, Studentdto>().ReverseMap(); // allow bi-directional mapping  , maps student to studentDTO and vise versa
+            CreateMap<Student, Student>().ReverseMap(); // maps student to student and viseVersa
+            CreateMap<RoleDto, Role>().ReverseMap(); // 
             CreateMap<RolePrivilegeDto, RolePrivilege>().ReverseMap();
             CreateMap<UserDto, User>().ReverseMap();
         }
